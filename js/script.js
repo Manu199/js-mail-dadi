e831048317618af363c2669abb8849192d909e55
Manu199 <manu199gh@gmail.com>
2023-09-12 15:51:13 +0200

    [add] input email

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*
+- Chiedere all'utente la sua email
+- Controllare che la email sia presente nella lista di chi può accedere
+- Stamapre un messaggio appropriato sull'esito del controllo
+- -----
+- Generare un numero random da 1 a 6, sia per il giocatore sia per il computer
+- Stabilire il vincitore, in base a chi fa il punteggio più alto
+*/ 
+var inputElement = document.getElementById("email").value;
+
+console.log("email");
