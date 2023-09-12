762f891419dc2de38f5d481b510cb775f1f5a7f2
Manu199 <manu199gh@gmail.com>
2023-09-12 16:02:01 +0200

    [add] get email in console.log  by pressing button

--- js/script.js
+++ js/script.js
@@ -6,6 +6,14 @@
 - Generare un numero random da 1 a 6, sia per il giocatore sia per il computer
 - Stabilire il vincitore, in base a chi fa il punteggio più alto
 */ 
-var inputElement = document.getElementById("email").value;
+const inputElement = document.getElementById('email');
+const buttonElement = document.createElement("send");
 
-console.log("email");
+
+send.addEventListener("click", function(){
+
+  const inputValue = inputElement.value;
+
+  console.log(inputValue);
+
+});
